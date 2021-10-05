93f150f62e6a18c4919e0549d3b1d5ddc832dd9d
t2iotautoamtion <51674088+t2iotautoamtion@users.noreply.github.com>
2021-10-05 10:02:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*var computer="2016";
+var comp1="2023";
+var comp_1="2023";
+var Computer="2000";
+var compModel="2020";
+ document.write(Computer);
+ document.write(comp1);
+ document.write(comp_1);
+ document.write(computer);
+ document.write(compModel);*/
+ var str_1= 'hello';
+ var int_1= 5.2+5.4;
+ document.write(int_1);
+var float_1= 5+5;
+document.write(float_1);
